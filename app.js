fa697dd8021e29fcbac12956e054cd25420c0b61
DimiKou <49120824+DimitrisKourk@users.noreply.github.com>
2024-02-02 15:21:34 +0000

    commit server reconnect

--- app.js
+++ app.js
@@ -105,13 +105,14 @@ app.post("/api/music", (req, res) => addSong(req, res));
 app.put("/api/music/:id", (req, res) => updateSong(req, res));
 app.delete("/api/music/:id", (req, res) => deleteSong(req, res));
 
-const connection = mysql.createConnection({
+credentials = {
   user: "sql11680320",
   host: "sql11.freesqldatabase.com",
   database: "sql11680320",
   password: "u3pzqNXXEZ",
   port: 3306,
-});
+};
+const connection = mysql.createConnection(credentials);
 
 connection.connect(function (err) {
   if (err) {
@@ -120,6 +121,30 @@ connection.connect(function (err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+function handleDisconnect() {
+  connection = mysql.createConnection(credentials); // Recreate the connection, since
+  // the old one cannot be reused.
+  connection.connect(function (err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
 
 const showResults = (response, error, results) => {
   if (error) {
